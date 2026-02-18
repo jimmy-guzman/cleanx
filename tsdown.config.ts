4f23cffc3e288b5f3c63760dcb6996f8b4637e4c
jimmy-guzman <hi@jimmy.codes>
2026-02-18 07:34:19 -0600

    fix: 🐛 bump `brace-expansion` to v5

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from "tsdown";
 
 export default defineConfig({
-  inlineOnly: [],
+  inlineOnly: [
+    "graphmatch",
+    "zeptomatch",
+    "grammex",
+    "fdir",
+    "balanced-match",
+    "brace-expansion",
+    "ignore",
+    "ms",
+    "picomatch",
+    "pathe",
+    "escalade",
+    "picomatch",
+    "js-yaml",
+    "jju",
+    "@manypkg/tools",
+    "@manypkg/get-packages",
+    "@manypkg/find-root",
+    "tinyglobby",
+  ],
   minify: true,
   noExternal: () => true,
   platform: "node",
